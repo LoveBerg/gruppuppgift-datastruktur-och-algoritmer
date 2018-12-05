779aaa33bf7bfb5cba0700728fd4d6e4db49fa24
Robert Sundh <37418090+surrelito@users.noreply.github.com>
2018-12-05 11:14:37 +0100

    new

--- Datastrukturer gruppuppgift/linkedList.h
+++ Datastrukturer gruppuppgift/linkedList.h
@@ -1,9 +1,50 @@
 #pragma once
+#include <iostream>
 
 template <typename T>
-class LinkedList {
+struct node
+{
+	T data;
+	node *next;
+};
+
+template <typename T>
+class LinkedList
+{
 private:
+	struct node *head, *tail;
 
 public:
-	gregrege
+
+	LinkedList()
+	{
+		head = NULL;
+		tail = NULL;
+	}
+	void AddNode(T n)
+	{
+		node *temp = new node;
+		temp->data = n;
+		temp->next = NULL;
+
+		if (head == NULL)
+		{
+			head = temp;
+			tail = temp;
+		}
+		else
+		{
+			tail->next = temp;
+			tail = tail->next;
+		}
+	}
 };
+
+int main()
+{
+	LinkedList<int> a;
+	a.AddNode(1);
+	a.AddNode(2);
+	return 0;
+
+}
