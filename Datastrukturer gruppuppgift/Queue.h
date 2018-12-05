71897a35363605ca97a75e82d65eb62386b3dae7
olgamedt <andicsson@gmail.com>
2018-12-05 14:50:40 +0100

    Abb
    Deleta queue

--- Datastrukturer gruppuppgift/Queue.h
+++ Datastrukturer gruppuppgift/Queue.h
@@ -1,8 +1,28 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
+template <class datatype>
 class Queue
 {
 public:
 	Queue(); // constructor
 	~Queue(); // destructor
+	void enqueue(datatype);
+	void dequeue(datatype &);
+	bool isEmpty() const;
+	
+	void destroyQueue();
+private:
+	struct qNode
+	{
+		datatype data;
+		struct qNode *next;
+	};
+	
+	qNode *front; // holds front of queue
+	qNode *back; // holds back of queue
+
+	int queueNum;
+
 };
