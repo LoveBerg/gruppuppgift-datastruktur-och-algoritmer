77d3b5d9cf98c5d053d77998b200e6b7e9997804
LoveBerg <43234984+LoveBerg@users.noreply.github.com>
2018-12-06 18:59:56 +0100

    Stack och tester till stack

--- Datastrukturer gruppuppgift/Stack.cpp
+++ Datastrukturer gruppuppgift/Stack.cpp
@@ -2,25 +2,4 @@
 #include <vector>
 
 
-template <class datatype>
-void Stack<datatype>::Pop() {
-	if (this->stackVector.size() == 0)
-		throw "Stack is empty!";
-	this->stackVector.erase(this->stackVector.begin());
-}
-
-
-template <class datatype>
-void Stack<datatype>::Push(datatype element) {
-	if(this->isEmpty == true)
-		this->isEmpty == false;
-	this->stackVector.push_back(element);
-}
-
-template <class datatype>
-datatype* Stack<datatype>::getElement() {
-	if (this->stackVector.size() == 0)
-		throw "Stack is empty!";
-	return *this->stackVector.begin();
-}
 
