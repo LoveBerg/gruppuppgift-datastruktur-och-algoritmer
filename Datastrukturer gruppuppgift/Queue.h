7193219eab9951bbb58a555148cfd7209343f5cb
olgamedt <andicsson@gmail.com>
2018-12-09 15:44:17 +0100

    Klar med queue
    Ska börja med tester

--- Datastrukturer gruppuppgift/Queue.h
+++ Datastrukturer gruppuppgift/Queue.h
@@ -6,10 +6,21 @@ template <class DataType>
 class Queue
 {
 public:
-	Queue(); // constructor
-
-	~Queue(); // destructor
-
+	Queue<DataType>::Queue()
+	{
+		head = tail = NULL;
+		count = 0;
+	}
+	Queue<DataType>::~Queue()
+	{
+		clear();
+	}
+	int size();
+	bool isEmpty();
+	void clear();
+	void enqueue(DataType value);
+	DataType dequeue();
+	DataType peek();
 private:
 
 	struct Cell
@@ -18,5 +29,59 @@ private:
 		Cell *next
 	};
 
+Cell *head;
+Cell *tail;
+int count;
 
 };
+
+template <typename DataType>
+int Queue<DataType>::size() 
+{
+	return count;
+}
+
+template <typename DataType>
+bool Queue<DataType>::isEmpty() {
+	return count == 0;
+}
+
+template <typename DataType>
+void Queue<DataType>::clear() {
+	while (count > 0) {
+		dequeue();
+	}
+}
+
+template <typename DataType>
+void Queue<DataType>::enqueue(DataType value) {
+	Cell *cp = new Cell;
+	cp->data = value;
+	cp->next = NULL;
+	if (head == NULL) {
+		head = Cell;
+	}
+	else {
+		tail->next = Cell;
+	}
+	tail = Cell;
+	count++;
+}
+
+template <typename DataType>
+DataType Queue<DataType>::dequeue() {
+	if (isEmpty()) error("Dequeue: Attempting to dequeue an empty queue");
+	Cell *cp = head;
+	DataType result = cp->data;
+	head = cp->next;
+	if (head == NULL) tail = NULL;
+	count--;
+	delete Cell;
+	return result;
+}
+
+template <typename DataType>
+DataType Queue<DataType>::peek() {
+	if (isEmpty()) error("peek: Attempting to peek at an empty queue");
+	return head->data;
+}
