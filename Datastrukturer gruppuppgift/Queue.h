20e453e4158dfb7ed9300952f892d23076c39849
olgamedt <andicsson@gmail.com>
2018-12-09 15:00:54 +0100

    Gör om
    Gör om min queue med egen länkad lista

--- Datastrukturer gruppuppgift/Queue.h
+++ Datastrukturer gruppuppgift/Queue.h
@@ -2,27 +2,21 @@
 #include <iostream>
 using namespace std;
 
-template <class datatype>
+template <class DataType>
 class Queue
 {
 public:
 	Queue(); // constructor
-	~Queue(); // destructor
-	void enqueue(datatype);
-	void dequeue(datatype &);
-	bool isEmpty() const;
-	
-	void destroyQueue();
-private:
-	struct qNode
-	{
-		datatype data;
-		struct qNode *next;
-	};
-	
-	qNode *front; // holds front of queue
-	qNode *back; // holds back of queue
 
-	int queueNum;
+	~Queue(); // destructor
+
+private:
+
+	struct Cell
+	{
+		DataType data;
+		Cell *next
+	};
+
 
 };
